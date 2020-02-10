176f03ddb72e2904d1f647f16844031f80ad5ff9
Patryk Chodacki <chodacki.pc@gmail.com>
2020-02-10 09:02:10 +0100

    trying to create p element while pushing content to array

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ const date = months[d.getMonth()] + " " + d.getDate() + " " + d.getFullYear();
 const timeHandler = document.querySelector(".timeHandler").innerHTML = time;
 const dateHandler = document.querySelector(".dateHandler").innerHTML = date;
 
+let todoArray = [];
+
+
+const buttonPushInput = () => {
+    let todoHandler = document.querySelector(".section3Input").value;
+    todoArray.push(todoHandler);
+    console.log(todoArray);
+    let newP = document.createElement('p').innerHTML = todoArray[0];
+    // for(let i; i=0; i++) {
+    //     (todoArray.length === i) ? document.createElement('p').innerHTML = todoArray[i] : document.createElement('p').innerHTML = "Coś nie gra";
+    // }
+    
+    
+}
+
