d8b6365193b1bf4074e497918cda2821e4f01b3f
Patryk Chodacki <chodacki.pc@gmail.com>
2020-02-10 12:28:23 +0100

    p is working, maby next some map on array to acces individual task

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const d = new Date();
 
-const time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+
+const workingClock = () => {
+    const d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    let time = h + ":" + m + ":" + s;
+    let timeHandler = document.querySelector(".timeHandler").innerHTML = time;
+}
+
+let interval = setInterval(workingClock, 1000);
+
+const killInterval = () => {
+    clearInterval(interval)
+}
+
+const checkTime = (i) => {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+    }
+
+// Working Clock
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const date = months[d.getMonth()] + " " + d.getDate() + " " + d.getFullYear();
 
-const timeHandler = document.querySelector(".timeHandler").innerHTML = time;
+
 const dateHandler = document.querySelector(".dateHandler").innerHTML = date;
 
 let todoArray = [];
@@ -16,11 +40,17 @@ const buttonPushInput = () => {
     let todoHandler = document.querySelector(".section3Input").value;
     todoArray.push(todoHandler);
     console.log(todoArray);
-    let newP = document.createElement('p').innerHTML = todoArray[0];
-    // for(let i; i=0; i++) {
-    //     (todoArray.length === i) ? document.createElement('p').innerHTML = todoArray[i] : document.createElement('p').innerHTML = "Coś nie gra";
-    // }
-    
-    
+    let newP = document.createElement('p');
+
+    let oldP = document.querySelector(".taskParagraph");
+
+    let parentP = oldP.parentNode
+
+
+
+    parentP.insertBefore(newP, oldP);
+    newP.innerHTML = todoArray;
+
 }
+
 
